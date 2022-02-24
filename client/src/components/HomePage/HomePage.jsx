38158d76965e203430ba392a139a2e47b9727c27
Mathieu <lebreton.mathieu2@gmail.com>
2022-02-24 14:44:18 +0100

    add page skeleton

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,10 +1,93 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import './homePage.scss';
 
-const HomePage = (
-  <div>
-    <h1>HomePage</h1>
-  </div>
-);
+function HomePage() {
+  return (
+    <>
+      <header className="header">
+        <img src="" alt="" className="header--img" />
+        <a to="/">Se connecter</a>
+        <h1 className="header-title">Phrase d'accroche</h1>
+      </header>
 
-export default HomePage;
+      <main className="main">
+        <section className="main--func">
+          <div className="func--content">
+            <img src="" alt="" className="func--img" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Asperiores minus fuga accusamus omnis nostrum iusto rerum sequi.
+            </p>
+          </div>
+
+          <div className="func--content">
+            <img src="" alt="" className="func--img" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Asperiores minus fuga accusamus omnis nostrum iusto rerum sequi.
+            </p>
+          </div>
+
+          <div className="func--content">
+            <img src="" alt="" className="func--img" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Asperiores minus fuga accusamus omnis nostrum iusto rerum sequi.
+            </p>
+          </div>
+        </section>
+
+        <section className="main--encart">
+          <h2 classeName="encart--title">Phrase d'accroche bis</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Odio placeat excepturi non quaerat fuga libero a incidunt corporis,
+            rerum obcaecati nemo, qui molestiae cupiditate velit aliquid inventore
+            sapiente harum repudiandae?
+          </p>
+          <img src="" alt="" className="encart--img" />
+        </section>
+
+        <section className="main--team">
+          <h2 className="team--title">Notre équipe</h2>
+          <div className="team--card">
+            <img src="" alt="" className="team--img" />
+            <p>
+              Lorem Ipsum
+            </p>
+          </div>
+
+          <div className="team--card">
+            <img src="" alt="" className="team--img" />
+            <p>
+              Lorem Ipsum
+            </p>
+          </div>
+
+          <div className="team--card">
+            <img src="" alt="" className="team--img" />
+            <p>
+              Lorem Ipsum
+            </p>
+          </div>
+
+          <div className="team--card">
+            <img src="" alt="" className="team--img" />
+            <p>
+              Lorem Ipsum
+            </p>
+          </div>
+
+          <div className="team--card">
+            <img src="" alt="" className="team--img" />
+            <p>
+              Lorem Ipsum
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default React.memo(HomePage);
