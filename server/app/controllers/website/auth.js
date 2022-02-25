da840dafb5915983583fe283c9f699805a98d0ba
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-02-25 19:02:23 +0100

    Add controller and test http

--- server/app/controllers/website/auth.js
+++ server/app/controllers/website/auth.js
@@ -1,9 +1,38 @@
-// const emailValidator = require('email-validator');
+const emailValidator = require('email-validator');
+const authDatamapper = require('../../models/website/auth');
+const { WebsiteError } = require('../../helpers/errorHandler');
 
 const controller = {
 
-  loginAction(req, res) {
-    res.send('Auth à gérer');
+  /**
+   * Login action
+   * ExpressMiddleware signature
+   * @param {object} req Express request object : {email, password}
+   * @param {object} res Express response object
+   * @returns {string} Route API JSON response
+   */
+  async loginAction(req, res) {
+    const isEmailValid = emailValidator.validate(req.body.email);
+
+    if (!req.body.email) {
+      return { error: 'L\'email obligatoire' };
+    }
+
+    if (!isEmailValid) {
+      return { error: 'Cet email n\'est pas valide' };
+    }
+
+    if (!req.body.password) {
+      return { error: 'Le mot de passe est obligatoire' };
+    }
+
+    const user = await authDatamapper.findOne(req.body.email, req.body.password);
+
+    if (!user) {
+      throw new WebsiteError(401, 'Email et/ou mot de passe invalide');
+    }
+
+    return res.json(user);
   },
 };
 
---
+++ server/app/models/website/auth.js
@@ -0,0 +1,16 @@
+const client = require('../../config/database');
+const { WebsiteError } = require('../../helpers/errorHandler');
+
+module.exports = {
+
+  async findOne(email, password) {
+    const result = await client.query('SELECT * FROM "employee" WHERE "email" = $1 AND "password" = $2', [email, password]);
+
+    if (result.rowCount === 0) {
+      return null;
+    }
+
+    return result.rows[0];
+  },
+
+};
