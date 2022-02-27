895bec1b077021ee3a67449ca7ee2be2bb072073
Mathieu <lebreton.mathieu2@gmail.com>
2022-02-27 21:56:10 +0100

    add bottomnav + links

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import DateRangeRoundedIcon from '@mui/icons-material/DateRangeRounded';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import './footer.scss';
 
 function Footer() {
   return (
-    <>
-
-    </>
+    <BottomNavigation
+      showLabels
+      sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <Link to="/"><BottomNavigationAction label="Gestion de plannings" icon={<DateRangeRoundedIcon />} /></Link>
+      <Link to="/"><BottomNavigationAction label="Mon compte" icon={<SettingsIcon />} /></Link>
+      <Link to="/"><BottomNavigationAction label="Se déconnecter" icon={<LogoutRoundedIcon />} /></Link>
+    </BottomNavigation>
   );
 }
 
