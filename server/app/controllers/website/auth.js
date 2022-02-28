329cb5ab46d7f1b546340ba55d237074f4a49424
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-02-28 10:29:47 +0100

    Fixed code HTTP

--- server/app/controllers/website/auth.js
+++ server/app/controllers/website/auth.js
@@ -14,22 +14,18 @@ const controller = {
   async loginAction(req, res) {
     const isEmailValid = emailValidator.validate(req.body.email);
 
-    if (!req.body.email) {
-      throw new WebsiteError(401, 'L\'email obligatoire');
+    if (!req.body.email || !req.body.password) {
+      throw new WebsiteError(400, 'L\'email et le mot de passe sont requis');
     }
 
     if (!isEmailValid) {
-      throw new WebsiteError(401, 'Cet email n\'est pas valide');
-    }
-
-    if (!req.body.password) {
-      throw new WebsiteError(401, 'Le mot de passe est obligatoire');
+      throw new WebsiteError(400, 'Cet email n\'est pas valide');
     }
 
     const user = await authDatamapper.findOne(req.body.email, req.body.password);
 
     if (!user) {
-      throw new WebsiteError(401, 'Email et/ou mot de passe invalide');
+      throw new WebsiteError(422, 'Email et/ou mot de passe invalide');
     }
 
     return res.json(user);
