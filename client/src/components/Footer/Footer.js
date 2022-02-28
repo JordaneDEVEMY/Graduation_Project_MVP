5ec33d702141ed52dde7ca19e0df3c4265da4eac
Mathieu <lebreton.mathieu2@gmail.com>
2022-02-28 10:38:59 +0100

    add legal mentions

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,34 +1,55 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import { Link } from 'react-router-dom';
+import { Typography, Box } from '@mui/material';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import DateRangeRoundedIcon from '@mui/icons-material/DateRangeRounded';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import './footer.scss';
 
 function Footer() {
   return (
-    <BottomNavigation
-      showLabels
-      sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-    >
-      {/* <Link to="/"> */}
-      <BottomNavigationAction label="Gestion de plannings" icon={<DateRangeRoundedIcon />} />
-      {/* </Link> */}
-      {/* <Link to="/"> */}
-      <BottomNavigationAction label="Mon compte" icon={<SettingsIcon />} />
-      {/* </Link> */}
-      {/* <Link to="/"> */}
-      <BottomNavigationAction label="Se déconnecter" icon={<LogoutRoundedIcon />} />
-      {/* </Link> */}
-    </BottomNavigation>
+    <footer>
+      <Box>
+        <Typography>
+          Mentions légales
+        </Typography>
+      </Box>
+
+      <BottomNavigation
+        showLabels
+        sx={{
+          display: {
+            xs: 'block',
+            md: 'none',
+          },
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        {/* <Link to="/"> */}
+        <BottomNavigationAction label="Plannings" icon={<DateRangeRoundedIcon />} />
+        {/* </Link> */}
+
+        {/* <Link to="/"> */}
+        <BottomNavigationAction disabled label="Personnel" icon={<BadgeRoundedIcon />} />
+        {/* </Link> */}
+
+        {/* <Link to="/"> */}
+        <BottomNavigationAction disabled label="Sites" icon={<EngineeringIcon />} />
+        {/* </Link> */}
+
+        {/* <Link to="/"> */}
+        <BottomNavigationAction disabled label="Clients" icon={<SupervisorAccountIcon />} />
+        {/* </Link> */}
+      </BottomNavigation>
+    </footer>
   );
 }
 
