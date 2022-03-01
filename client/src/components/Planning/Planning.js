553ae1df35ac55f0fb836816c1e27e1c2d64af39
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-01 11:53:20 +0100

    add windowWidth hook

--- client/src/components/Planning/Planning.js
+++ client/src/components/Planning/Planning.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import PropTypes from 'prop-types';
+import useWindowWidth from '../../hooks/useWindowWidth';
+import Carousel from '../Carousel/Carousel';
 import './planning.scss';
 
 function Planning({
   isAdmin,
 }) {
+  const [displayCarousel, setDisplayCarousel] = useState(false);
+
+  const theme = useTheme();
+  const width = useWindowWidth();
+  const minWidth = theme.breakpoints.values.sm;
+
+  useEffect(() => {
+    if (width < minWidth) {
+      setDisplayCarousel(true);
+    }
+
+    if (width >= minWidth) {
+      setDisplayCarousel(false);
+    }
+  }, [width]);
+
   return (
-    <Typography paragraph sx={{ color: 'text.primary' }}>
-      {`Planning en mode ${isAdmin ? 'admin' : 'user'}`}
-    </Typography>
+    <>
+      <Typography paragraph sx={{ color: 'text.primary' }}>
+        {`Planning en mode ${isAdmin ? 'admin' : 'user'}`}
+      </Typography>
+      {displayCarousel && (
+      <Carousel />
+      )}
+    </>
   );
 }
 
