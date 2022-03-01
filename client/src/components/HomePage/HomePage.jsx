0e8f6c981be461931cb36937b4cd6696cd030741
Mathieu <lebreton.mathieu2@gmail.com>
2022-03-01 13:16:34 +0100

    change background color source

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -51,7 +51,7 @@ function HomePage() {
             xs: '50vh',
             md: '100vh',
           },
-          backgroundColor: theme.palette.common,
+          backgroundColor: theme.palette.background.default,
         }}
         >
           {/*
@@ -163,7 +163,7 @@ function HomePage() {
       <Box
         component="main"
         sx={{
-          backgroundColor: theme.palette.common,
+          backgroundColor: theme.palette.background.default,
         }}
       >
         {/*
