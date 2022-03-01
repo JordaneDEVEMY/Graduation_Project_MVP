76a1b9dbbb0556b8883d8c30ca2b4204a65a5a51
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-01 12:01:01 +0100

    add windowWidth hook and use it in planning component

--- client/src/components/Planning/Planning.js
+++ client/src/components/Planning/Planning.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
-import { Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import PropTypes from 'prop-types';
-import useWindowWidth from '../../hooks/useWindowWidth';
-=======
-import React from 'react';
 import { Box, Typography } from '@mui/material';
-import PropTypes from 'prop-types';
->>>>>>> 0e85500af2721a7790f2dc1a298c5e63fd61ffc1
+import useWindowWidth from '../../hooks/useWindowWidth';
 import Carousel from '../Carousel/Carousel';
 import './planning.scss';
 
