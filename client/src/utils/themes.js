bd501a8ae1d695326b0d7c437c5fb89c4eecc7ca
Pascal Tassel <tasselp@gmail.com>
2022-03-02 10:35:49 +0100

    create themes module

---
+++ client/src/utils/themes.js
@@ -0,0 +1,22 @@
+const themes = {
+  dark: {
+    palette: {
+      background: {
+        default: '#121212',
+        paper: '#121212',
+        component: '#1e1e1e',
+      },
+    },
+  },
+  light: {
+    palette: {
+      background: {
+        default: '#fff',
+        paper: '#fff',
+        component: '#f7f7f7',
+      },
+    },
+  },
+};
+
+export default themes;
