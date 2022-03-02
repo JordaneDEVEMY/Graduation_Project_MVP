d475cd8e913fc15d85246a31ca6da1429fe7212e
Pascal Tassel <tasselp@gmail.com>
2022-03-02 11:53:25 +0100

    eval window width in layout

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,19 +1,25 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 import PropTypes from 'prop-types';
 import Sidebar from '../Sidebar/Sidebar';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import useWindowWidth from '../../hooks/useWindowWidth';
 import './layout.scss';
 import './admin_layout.scss';
 
 function Layout({
   isAdmin,
 }) {
+  const theme = useTheme();
+  const windowWidth = useWindowWidth();
+  const minWidth = theme.breakpoints.values.sm;
+
   return (
     <div className="wrapper">
-      {isAdmin
+      {isAdmin && (windowWidth >= minWidth)
         && (<Sidebar />)}
       <Main>
         <Outlet />
