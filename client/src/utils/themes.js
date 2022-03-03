ee78039891aa9c9f595fce62b74d35b11492b420
Pascal Tassel <tasselp@gmail.com>
2022-03-03 20:10:39 +0100

    responsive font-size

--- client/src/utils/themes.js
+++ client/src/utils/themes.js
@@ -37,21 +37,27 @@ const getTheme = (mode) => createTheme({
   typography: {
     h1: {
       fontSize: '3rem',
+      marginBottom: '.5em',
     },
     h2: {
       fontSize: '2rem',
+      marginBottom: '.5em',
     },
     h3: {
       fontSize: '1.75rem',
+      marginBottom: '.5em',
     },
     h4: {
       fontSize: '1.5rem',
+      marginBottom: '.5em',
     },
     h5: {
       fontSize: '1.25rem',
+      marginBottom: '.5em',
     },
     h6: {
       fontSize: '1rem',
+      marginBottom: '.5em',
     },
   },
 });
