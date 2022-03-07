b4ae2cb3020cd76dac4b77d9c86fe763e76f20b5
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-07 19:54:37 +0100

    add requestSiteInformations action into siteMiddleware

--- client/src/middlewares/siteMiddleware.js
+++ client/src/middlewares/siteMiddleware.js
@@ -3,6 +3,42 @@ import {
 } from '../requests/siteRequest';
 import * as actions from '../actions';
 
-const siteMiddleware = (store) => (next) => async (action) => {};
+const siteMiddleware = (store) => (next) => async (action) => {
+  switch (action.type) {
+    case actions.REQUEST_SITE_INFORMATIONS: {
+      const { site } = store.getState();
+      const response = await getOneSite(site.id);
+      if (response.status === 200) {
+        const {
+          id,
+          name,
+          adress,
+          zip_code: zipCode,
+          manager_name: managerName,
+          estimated_duration: estimatedDuration,
+          company_id: companyId,
+          created_at: createdAt,
+          updated_at: updatedAt,
+        } = response.data;
+
+        store.dispatch(actions.actionGetSiteInformations({
+          id,
+          name,
+          adress,
+          zipCode,
+          managerName,
+          estimatedDuration,
+          companyId,
+          createdAt,
+          updatedAt,
+        }));
+      }
+      return;
+    }
+    default: {
+      next(action);
+    }
+  }
+};
 
 export default siteMiddleware;
