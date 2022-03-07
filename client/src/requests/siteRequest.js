6c4ad5db1993c78f6efcd1494027d794457f8ef9
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-07 18:40:01 +0100

    add getOneSite request

--- client/src/requests/siteRequest.js
+++ client/src/requests/siteRequest.js
@@ -1,1 +1,10 @@
 import apiAxios from './index';
+
+export async function getOneSite(id) {
+  try {
+    const response = apiAxios.get(`/api/admin/site/${id}`);
+    return response;
+  } catch (err) {
+    return err.response;
+  }
+}
