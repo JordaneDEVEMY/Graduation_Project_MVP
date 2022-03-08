e4870502fa9448160fb17e800bf6174af34ba983
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-08 11:38:42 +0100

    admin planning request ok

--- client/src/middlewares/adminPlanningMiddleware.js
+++ client/src/middlewares/adminPlanningMiddleware.js
@@ -3,13 +3,14 @@ import * as actions from '../actions';
 
 const adminPlanningMiddleware = (store) => (next) => async (action) => {
   switch (action.type) {
-    case actions.GET_USER_PLANNING: {
+    case actions.REQUEST_ADMIN_PLANNING: {
       const response = await requestAdminPlanning();
 
       if (response.status === 200) {
         const { weekStart, planning } = response.data;
 
-        store.dispatch(actions.actionGetAdminPlanning(weekStart, planning));
+        store.dispatch(actions.actionGetAdminPlanning({ weekStart, planning }));
+        store.dispatch(actions.actionGetUserPlanning());
       }
       return;
     }
