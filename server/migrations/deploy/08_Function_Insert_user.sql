2656a96f5ddca34702257e364e8302f65a7fb0a2
hichamC31300 <hicham.chrita@gmail.com>
2022-03-08 11:13:44 +0100

    Add SQL function to create new employee

--- server/migrations/deploy/08_Function_Insert_user.sql
+++ server/migrations/deploy/08_Function_Insert_user.sql
@@ -2,6 +2,44 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+CREATE OR REPLACE FUNCTION insert_user (u json) RETURNS employee AS $$
+	
+    INSERT INTO employee
+        (
+            "firstname",
+            "lastname",
+            "email",
+            "password",
+            "phone_number",
+            "mobile_number",
+            "address",
+            "zip_code",
+            "date_of_birth",
+            "social_security_number",
+            "starting_date",
+            "function",
+            "avatar",
+            "role_application",
+            "employee_qualification_id"
+        )
+        VALUES (
+            (u->> 'firstname')::text,
+            (u->> 'lastname')::text,
+            (u->> 'email')::text,
+            (u->> 'password')::text,
+            (u->> 'phone_number')::text,
+            (u->> 'mobile_number')::text,
+            (u->> 'address')::text,
+            (u->> 'zip_code')::int,
+            (u->> 'date_of_birth')::timestamptz,
+            (u->> 'social_security_number')::text,
+            (u->> 'starting_date')::timestamptz,
+            (u->> 'function')::text,
+            (u->> 'avatar')::text,
+            (u->> 'role_application')::text,
+            (u->> 'employee_qualification_id')::int
+        ) RETURNING *;
+
+$$ LANGUAGE sql;
 
 COMMIT;
