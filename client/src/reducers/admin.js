fa4c25c861fe42e0c724a865e68da7df8e5f3388
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-08 11:00:25 +0100

    admin reducer created

---
+++ client/src/reducers/admin.js
@@ -0,0 +1,16 @@
+/* eslint-disable default-param-last */
+import * as actions from '../actions';
+
+const initialState = {
+  weekStart: '',
+  planning: [],
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
