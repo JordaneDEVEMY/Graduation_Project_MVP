42e3b045fbb68365f5fb841d69cb5f549b221f0a
Jordane DEVÉMY <76247941+JordaneDEVEMY@users.noreply.github.com>
2022-03-08 12:12:05 +0100

    Change revert file for drop function

--- server/migrations/revert/08_Function_Insert_user.sql
+++ server/migrations/revert/08_Function_Insert_user.sql
@@ -2,46 +2,6 @@
 
 BEGIN;
 
-CREATE FUNCTION "insert_user" ( 
-    firstname text, 
-    lastname text, 
-    email text, 
-    password text, 
-    phone_number text, 
-    mobile_number text, 
-    address text, 
-    zip_code int, 
-    date_of_birth timestamptz, 
-    social_security_number text, 
-    starting_date timestamptz, 
-    function text, 
-    avatar text, 
-    role_application text, 
-    employee_qualification_id int
-    ) RETURNS int AS $$
-    INSERT INTO "employee" 
-        (
-          "firstname",
-          "lastname",
-          "email",
-          "password",
-          "phone_number",
-          "mobile_number",
-          "address",
-          "zip_code",
-          "date_of_birth",
-          "social_security_number",
-          "starting_date",
-          "function",
-          "avatar",
-          "role_application",
-          "employee_qualification_id"
-        )
-        VALUES (
-          $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
-        )
-        RETURNING*
-
-$$ LANGUAGE 'plpgsql';
+DROP FUNCTION "insert_user" ( 
 
 COMMIT;
