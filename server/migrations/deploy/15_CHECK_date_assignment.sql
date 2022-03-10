a2959eb4c4845a7ad0813b1e342fd6637a28176e
hichamC31300 <hicham.chrita@gmail.com>
2022-03-10 10:37:17 +0100

    Add check constraint starting-ending date

---
+++ server/migrations/deploy/15_CHECK_date_assignment.sql
@@ -0,0 +1,7 @@
+-- Deploy olleks:15_CHECK_date_assignment to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ server/migrations/revert/15_CHECK_date_assignment.sql
@@ -0,0 +1,7 @@
+-- Revert olleks:15_CHECK_date_assignment from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
