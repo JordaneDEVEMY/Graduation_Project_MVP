1636847428dee62c4d9b2b2c6787544eb5bd91d2
hichamC31300 <hicham.chrita@gmail.com>
2022-03-10 15:48:20 +0100

    Add function to update assignment

---
+++ server/migrations/revert/16_Function_Update_Assignment.sql
@@ -0,0 +1,7 @@
+-- Revert olleks:16_Function_Updtae_Assignment from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ server/migrations/verify/16_Function_Update_Assignment.sql
@@ -0,0 +1,7 @@
+-- Verify olleks:16_Function_Updtae_Assignment on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
