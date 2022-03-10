5b1aca9dcd5d86d4b15603fcd1bdf7638828b8a1
hichamC31300 <hicham.chrita@gmail.com>
2022-03-10 15:50:34 +0100

    Add verify sqitch function update assignment

--- server/migrations/verify/16_Function_Update_Assignment.sql
+++ server/migrations/verify/16_Function_Update_Assignment.sql
@@ -2,6 +2,19 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+SELECT update_assignment(
+'
+	{
+		"id": "23",
+		"starting_date": "2022-03-07 00:00:00+00",
+		"ending_date": "2022-03-13 00:00:00+00",
+		"color": "#F0F",
+		"position":0,
+		"employee_id": 7,
+		"absence_id": null,
+		"site_id": "11"
+	}
+	'
+);
 
 ROLLBACK;
