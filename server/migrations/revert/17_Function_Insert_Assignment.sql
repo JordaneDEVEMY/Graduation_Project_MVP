15c83140b4ca65b9e0526aed00b7c97a202fc205
hichamC31300 <hicham.chrita@gmail.com>
2022-03-10 20:32:43 +0100

    Add function insert assignment

---
+++ server/migrations/revert/17_Function_Insert_Assignment.sql
@@ -0,0 +1,7 @@
+-- Revert olleks:17_Function_Insert_Assignment from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ server/migrations/verify/17_Function_Insert_Assignment.sql
@@ -0,0 +1,7 @@
+-- Verify olleks:17_Function_Insert_Assignment on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
