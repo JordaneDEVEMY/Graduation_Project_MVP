816935a2c7c5bc1e7d78b1a8edc9de1700ee2796
Mathieu <lebreton.mathieu2@gmail.com>
2022-03-10 13:11:59 +0100

    create state error on profilPage component

--- client/src/components/FieldForms/TextInput.js
+++ client/src/components/FieldForms/TextInput.js
@@ -11,19 +11,38 @@ function TextInput({
   label,
   defaultValue,
   value,
+  errorDisplay,
 }) {
   return (
-    <TextField
-      sx={{ width: '400px' }}
-      required
-      type={type}
-      name={nameValue}
-      value={value}
-      label={label}
-      defaultValue={defaultValue}
-      variant="outlined"
-      onChange={(event) => handleChange(nameValue, event.target.value)}
-    />
+    <>
+      <TextField
+        sx={{ width: '400px' }}
+        required
+        type={type}
+        name={nameValue}
+        value={value}
+        label={label}
+        defaultValue={defaultValue}
+        variant="outlined"
+        onChange={(event) => handleChange(nameValue, event.target.value)}
+      />
+
+      { errorDisplay && (
+      <TextField
+        sx={{ width: '400px' }}
+        required
+        error
+        type={type}
+        name={nameValue}
+        value={value}
+        label={label}
+        defaultValue={defaultValue}
+        variant="outlined"
+        onChange={(event) => handleChange(nameValue, event.target.value)}
+      />
+      ) }
+
+    </>
   );
 }
 
@@ -34,6 +53,7 @@ TextInput.propTypes = {
   value: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   defaultValue: PropTypes.string.isRequired,
+  errorDisplay: PropTypes.string.isRequired,
 };
 
 export default React.memo(TextInput);
