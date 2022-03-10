83f38dfc1a72f750426bfae5a44c9ad6e6bd4e1c
Mathieu <lebreton.mathieu2@gmail.com>
2022-03-10 14:42:35 +0100

    creation visual Forgot-password page

--- client/src/components/ForgotPassword/ForgotPassword.js
+++ client/src/components/ForgotPassword/ForgotPassword.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import {
+  Box, Typography, Grid, Button, Divider, useTheme,
+} from '@mui/material';
+import TextInput from '../FieldForms/TextInput';
 
 function ForgotPassword() {
+  const theme = useTheme();
+
   return (
-    <div>ForgotPassword</div>
+    <Box sx={{ p: theme.spacing(2), textAlign: 'center' }}>
+      <Box
+        component="header"
+      >
+        <Typography variant="h1">
+          Mot de passe oublié
+        </Typography>
+        <Divider />
+      </Box>
+      <Box
+        component="div"
+        sx={{ marginTop: theme.spacing(2) }}
+      >
+        <form>
+          <Grid container rowSpacing={2}>
+            <Grid item xs={12}>
+              <TextInput
+                type="email"
+                nameValue="email"
+                label="Entrez votre adresse e-mail"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                sx={{ marginBottom: theme.spacing(2) }}
+                type="submit"
+                variant="outlined"
+              >
+                Récupérer mon mot de passe
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Box>
   );
 }
 
