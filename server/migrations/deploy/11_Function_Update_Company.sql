1d821aad232abf5e18a1e6847753fa0f29e3ef2e
hichamC31300 <hicham.chrita@gmail.com>
2022-03-10 14:07:26 +0100

    Add updated_at field on update

--- server/migrations/deploy/11_Function_Update_Company.sql
+++ server/migrations/deploy/11_Function_Update_Company.sql
@@ -7,7 +7,8 @@ CREATE OR REPLACE FUNCTION update_company(json) RETURNS void AS $$
 	SET name = ($1 ->> 'name')::text,
 		address = ($1 ->> 'address')::text,
 		zip_Code = ($1 ->> 'zip_code')::int,
-		type = ($1 ->> 'type')::text
+		type = ($1 ->> 'type')::text,
+		updated_at = now()
 	WHERE id = ($1->>'id')::int;
 $$ LANGUAGE sql;
 
