58d0dec8e2689dc5f79ca87c1fdf43f554ff395a
hichamC31300 <hicham.chrita@gmail.com>
2022-03-10 20:35:23 +0100

    Verify function insert assignment sqitch plan

--- server/migrations/verify/17_Function_Insert_Assignment.sql
+++ server/migrations/verify/17_Function_Insert_Assignment.sql
@@ -2,6 +2,18 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+SELECT insert_assignment(
+	'{
+		"starting_date": "2022-03-17 00:00:00+00",
+		"ending_date": "2022-03-22 00:00:00+00",
+		"color": "#F0F",
+		"position": 0,
+		"visibility": true,
+		"employee_id": 24,
+		"absence_id": null,
+		"site_id": 8					
+	}'
+
+)
 
 ROLLBACK;
