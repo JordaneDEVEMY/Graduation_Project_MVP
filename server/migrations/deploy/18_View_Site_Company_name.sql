a4a455d2c8e7847839bb755f1ece0b99862ae62e
hichamC31300 <hicham.chrita@gmail.com>
2022-03-14 20:26:48 +0100

    Bug fix commit

---
+++ server/migrations/deploy/18_View_Site_Company_name.sql
@@ -0,0 +1,21 @@
+-- Deploy olleks:18_View_Site_Company_name to pg
+
+BEGIN;
+
+CREATE VIEW get_site_with_company_name AS
+		SELECT 
+		"site"."id", 
+		"site"."name", 
+		"site"."address", 
+		"site"."zip_code", 
+		"site"."manager_name", 
+		"site"."estimated_duration", 
+		"site"."company_id" AS company_id,
+		json_build_object(
+			'company_id', "company"."id",
+			'company_name', "company"."name"
+		) AS company
+	FROM "site"
+	LEFT JOIN "company" ON "company"."id" = "site"."company_id";
+
+COMMIT;
---
+++ server/migrations/verify/18_View_Site_Company_name.sql
@@ -0,0 +1,7 @@
+-- Verify olleks:18_View_Site_Company_name on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
