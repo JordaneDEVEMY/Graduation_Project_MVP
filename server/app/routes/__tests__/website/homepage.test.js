73cb001292626cc713e1b11aa65d31c093a91a73
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-03-15 10:01:51 +0100

    gitignore coverage and homepage test

---
+++ server/app/routes/__tests__/website/homepage.test.js
@@ -0,0 +1,12 @@
+// Using SuperTest for local HTTP request management.
+const request = require('supertest');
+const app = require('../../..');
+
+describe('Website documentation home page route test', () => {
+  test('responds to /', async () => {
+    const res = await request(app).get('/');
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('View');
+  });
+});
