3e050f73284cb58d900a3b77a551ec29d02a9fe5
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-03-17 12:21:47 +0100

    change on check token action

--- server/app/routes/website/checkToken.js
+++ server/app/routes/website/checkToken.js
@@ -1,8 +1,5 @@
 const express = require('express');
 
-const validate = require('../../validation');
-const checkTokenSchema = require('../../validation/checkTokenSchema');
-
 const { websiteAuth } = require('../../controllers');
 const controllerHandler = require('../../helpers/websiteControllerHandler');
 
@@ -14,12 +11,11 @@ router
    * POST /check-token
    * @summary Connect on website
    * @tags 1.Authentification
-   * @param {string} request.body.required - Token
    * @return {AuthUser} 200 - success response - application/json
    * @return {WebsiteError} 400 - Bad request response - application/json
    * @return {WebsiteError} 401 - Invalid Token - application/json
    * @return {WebsiteError} 500 - Internal server error - application/json
    */
-  .post(validate('body', checkTokenSchema), controllerHandler(websiteAuth.checkTokenAction));
+  .post(controllerHandler(websiteAuth.checkTokenAction));
 
 module.exports = router;
