3060209bfb82d2c65500f9b1a8ea2f6a5cddc617
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-03-17 11:52:15 +0100

    check token route

--- server/app/controllers/website/authController.js
+++ server/app/controllers/website/authController.js
@@ -2,6 +2,7 @@ const emailValidator = require('email-validator');
 // const bcrypt = require('bcryptjs');
 const { generateToken } = require('../../helpers/generateToken');
 const authDatamapper = require('../../models/website/auth');
+const userDatamapper = require('../../models/user');
 const { WebsiteError } = require('../../helpers/errorHandler');
 
 const controller = {
@@ -40,6 +41,39 @@ const controller = {
       });
     } else {
       throw new WebsiteError(401, 'Email or password invalid');
+    }
+    // }
+  },
+
+  /**
+   * Login action
+   * ExpressMiddleware signature
+   * @param {object} req Express request object : {email, password}
+   * @param {object} res Express response object
+   * @returns {user} Route API JSON response
+   */
+  async checkTokenAction(req, res) {
+    const { id, token } = req.body;
+
+    if (!token) {
+      throw new WebsiteError(401, 'Token is missing, Unauthorized Access');
+    }
+
+    const user = await userDatamapper.findByPk(id);
+
+    // if (user && (await bcrypt.compare(password, user.password))) {
+    if (user) {
+      res.json({
+        id: user.id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+        avatar: user.avatar,
+        role_application: user.role_application,
+        token: generateToken(user.id, '24h'),
+      });
+    } else {
+      throw new WebsiteError(401, 'Invalid Token');
     }
     // }
   },
