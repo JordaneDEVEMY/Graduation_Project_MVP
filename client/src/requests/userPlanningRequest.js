ccbc3470a6c30220a71c9cf697273ac6b7d619e1
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-18 09:38:42 +0100

    delete console.log userPlanningRequest

--- client/src/requests/userPlanningRequest.js
+++ client/src/requests/userPlanningRequest.js
@@ -4,7 +4,6 @@ import apiAxios from './index';
 export async function requestUserPlanning(id) {
   try {
     const response = await apiAxios.get(`/api/user/${id}`);
-    console.log(response);
     return response;
   } catch (err) {
     return err.response;
@@ -14,7 +13,6 @@ export async function requestUserPlanning(id) {
 export async function updateUserInformations(id, userDatas) {
   try {
     const response = await apiAxios.patch(`/api/user/${id}/profil`, userDatas);
-    console.log('updateUserInfos', response);
     return response;
   } catch (err) {
     return err.response;
